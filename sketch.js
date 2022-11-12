2ace043bd9509f3b88b8161343d6080fd4a1c6f1
Alicia Breinke <leeshia12@gmail.com>
2022-11-12 14:48:57 -0800

    update

--- sketch.js
+++ sketch.js
@@ -1,18 +1,16 @@
 
-let x = 500;
 function setup() {
-  mySound = loadSound('assets/birds.mp3');
   createCanvas(500, 500);
   background(0, 0, 0);
-  stroke(237,170,166);
-  strokeWeight(25);
 }
 
 function draw(){
    line(mouseX, mouseY, 150, 150);
-    }
-   function mouseMoved(){
-     if (mouseX > 250){
-       stroke(4,196,187);
-     }
+   stroke(237,170,166);
+   strokeWeight(25);
    }
+   function mouseMoved(){
+   if (mouseX > 250){
+   stroke(4,196,187);
+   }
+}
