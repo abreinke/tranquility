38777e65f66a63bd7aa96eee1f591e02b541f865
Alicia Breinke <leeshia12@gmail.com>
2022-11-12 15:01:12 -0800

    update

--- sketch.js
+++ sketch.js
@@ -2,15 +2,16 @@
 function setup() {
   createCanvas(500, 500);
   background(0, 0, 0);
+  stroke(237,170,166);
+  strokeWeight(25);
 }
 
 function draw(){
+  if (mousePressed)(){
   line(mouseX, mouseY, pmouseX, pmouseY);
-  stroke(237,170,166);
-  strokeWeight(25);
-   }
+  }
    function mouseMoved(){
    if (mouseX > 250){
    stroke(4,196,187);
-   }
+  }
 }
